2ff81388edbfa30b338ce66a14d536b53d7801bf
siddharthp-wal <siddharth.p@westagilelabs.com>
2020-03-23 10:31:48 +0530

    initial save

--- src/App.js
+++ src/App.js
@@ -1,24 +1,99 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+// // import SignUp from './component/SignUp/Index';
+// import './App.css';
+// import 'antd/dist/antd.css';
+// import { Menu} from 'antd';
+// import {Router, Switch ,Link, Route} from 'react-router-dom';
+// // import SignUp from './component/SignUp/SignUp'
 
-function App() {
+// class App extends React.Component {
+//   state = {
+//     current: 'mail',
+//   };
+
+//   handleClick = e => {
+//     console.log('click ', e);
+//     this.setState({
+//       current: e.key,
+//     });
+//   };
+
+//   render() {
+//     return (
+//     <div className="App">
+//       <Router>
+//       <Menu onClick={this.handleClick} selectedKeys={[this.state.current]} mode="horizontal">
+//         <Menu.Item >
+//         <Link to="/signup">Signup</Link>
+//         </Menu.Item>
+//         <Menu.Item key="app">
+//          SignIn
+//         </Menu.Item>
+//       </Menu>
+//         <div>
+          
+//         </div>
+//         <Switch>
+//           <Route path="/signup">
+//             {/* component */}
+//             {/* <SignUp/> */}
+//           </Route>
+//         </Switch>
+//       </Router>
+//     </div>
+//     );
+//   }
+// }
+
+// export default App;
+import React, { useState } from 'react';
+import 'antd/dist/antd.css';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem
+} from 'reactstrap';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import SignUp from './component/SignUp/SignUp'
+
+const App = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">Activity Tracker</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              {/* <NavLink href="/components/"></NavLink> */}
+            </NavItem>
+            <NavItem>
+              <Link to="/signup">SignUp</Link>
+              {/* <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink> */}
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+      
+        <Switch>
+          <Route path="/signup">
+            <SignUp></SignUp>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
